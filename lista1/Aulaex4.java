c2e4fd2fc6e957d5e3997eb37c44440434ebd212
Carlos Magno <csymagno@gmail.com>
2026-03-28 20:52:47 -0300

    Alternativa ao exercicio
    Alternativa legal feita pelo frança

--- lista1/Aulaex4.java
+++ lista1/Aulaex4.java
@@ -12,3 +12,34 @@ public class Aulaex4 {
         teclado.close();
     }
 }
+
+
+
+
+Alterinativa:
+
+
+import java.util.Scanner;
+
+public class ex4 {
+    public static void main(String [] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite uma quantia");
+        int valor = scanner.nextInt();
+
+        int[] quantia = new int[6];
+        int[] moedas = {100, 50, 25, 10, 5, 1};
+    
+        for(int i = 0; i < 6; i++){
+            if(valor >= moedas[i]){
+                quantia[i] = valor / moedas[i];
+                valor %= moedas[i];
+            }
+        }
+
+        for(int i = 0; i < 6; i++){
+            System.out.println(quantia[i]);
+        }
+    }
+}
